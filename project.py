333d1ca2c5cafe6a8cd182f42a4419828ea351bf
Jibbril Ndaw Berbres <jibbril.b.n@gmail.com>
2021-04-05 16:31:18 +0200

    Add hydro constraints

--- project.py
+++ project.py
@@ -52,9 +52,6 @@ from pyomo.environ import *
 import pandas as pd
 import matplotlib.pyplot as plt
 
-
-model = ConcreteModel()
-
 # CONTROL VARIABLES
 batteryOn = False
 
@@ -62,15 +59,39 @@ batteryOn = False
 # DATA
 countries = ['DE', 'DK', 'SE']
 techs = ['Wind', 'PV', 'Gas', 'Hydro', 'Battery']
-# you could also formulate efficiency as a time series with the capacity factors for PV and wind
 efficiency = {'Wind': 1, 'PV': 1, 'Gas': 0.4, 'Hydro': 1, 'Battery': 0.9}
-
 discountrate = 0.05
 
+# READ CSV
+print("========================= START LOADING DATA =========================")
 input_data = pd.read_csv('data/TimeSeries.csv', index_col=[0])
+cap_max_data = pd.read_csv('data/capMax.csv', index_col=[0])  # MWh
+cost_data = pd.read_csv('data/costs.csv', index_col=[0])
+print("========================= DATA LOADED =========================")
 
 
-# TIME SERIES HANDLING
+# UTILITY FUNCTIONS
+def annualize_cost(tech):
+    lifetime = cost_data.loc[tech]["lifetime"]
+    return discountrate/(1-1/(1+discountrate) ^ lifetime)
+
+
+def investment_cost(model):
+    sum(max([model.prod[node, tech, t] for t in model.time])*cost_data.loc[tech][investment_cost]
+        * annualize_cost(tech) for tech in model.gens for node in model.nodes)
+
+
+def capacity_max(model, n, g):
+    capMax = {}
+    if g in cap_max_data.columns:
+        capMax[n, g] = float(cap_max_data[g].loc[cap_max_data.index == n])
+        return 0.0, capMax[n, g]
+    elif g == 'Battery' and not batteryOn:
+        return 0.0, 0.0
+    else:
+        return 0.0, None
+
+
 def demandData():
     demand = {}
     for n in model.nodes:
@@ -79,33 +100,24 @@ def demandData():
     return demand
 
 
+print("========================= CREATE MODEL =========================")
+model = ConcreteModel()
+
+print("========================= CREATE SETS =========================")
 # SETS
 model.nodes = Set(initialize=countries, doc='countries')
 model.time = Set(initialize=input_data.index, doc='hours')
 model.gens = Set(initialize=techs, doc="Technologies")
 
-
+print("========================= SET PARAMETERS =========================")
 # PARAMETERS
 model.demand = Param(model.nodes, model.time, initialize=demandData())
 model.efficiency = Param(
     model.gens, initialize=efficiency, doc='Conversion efficiency')
 
 
+print("========================= CREATE VARIABLES =========================")
 # VARIABLES
-capMaxdata = pd.read_csv('data/capMax.csv', index_col=[0])  # MWh
-
-
-def capacity_max(model, n, g):
-    capMax = {}
-    if g in capMaxdata.columns:
-        capMax[n, g] = float(capMaxdata[g].loc[capMaxdata.index == n])
-        return 0.0, capMax[n, g]
-    elif g == 'Battery' and not batteryOn:
-        return 0.0, 0.0
-    else:
-        return 0.0, None
-
-
 model.prod = Var(model.nodes, model.gens, model.time,
                  domain=NonNegativeReals,
                  doc="Production")
@@ -113,7 +125,10 @@ model.capa = Var(model.nodes, model.gens,
                  bounds=capacity_max, doc='Generator cap')
 
 
+print("========================= SET CONSTRAINTS =========================")
 # CONSTRAINTS
+
+
 def production_capacity_rule(model, nodes, gens, time):
     return model.prod[nodes, gens, time] <= model.capa[nodes, gens]
 
@@ -129,11 +144,39 @@ def demand_rule(model, nodes, gens, time):
 model.demand_constraint = Constraint(model.nodes, model.gens,
                                      model.time, rule=demand_rule)
 
+
+print("========================= SET OBJECTIVE FUNCTION =========================")
 # OBJECTIVE FUNCTION
 
 
+def objective_rule_1(model):
+    total = {}
+
+    for tech in model.gens:
+        total[tech] = 0
+
+    for node in model.nodes:
+        for tech in model.gens:
+            for t in model.time:
+                prod = model.prod[node, tech, t].value
+                vc = cost_data.loc[tech]["variable_cost"]
+                fc = cost_data.loc[tech]["fuel_cost"]
+                ac = annualize_cost(prod, tech)
+
+                total[tech] += prod * \
+                    (cost_data.loc[tech]["variable_cost"] +
+                     cost_data.loc[tech]["fuel_cost"])
+
+    for tech in model.gens:
+        val = total[tech]
+        if (val > 0):
+            total[tech] += annualize_cost(val, tech)
+
+    return sum(total.values())
+
+
 def objective_rule(model):
-    return 1
+    return sum(model.prod[node, tech, t] * (cost_data.loc[tech]["variable_cost"] + cost_data.loc[tech]["fuel_cost"]) for node in model.nodes for tech in model.gens for t in model.time) + investment_cost(model)
 
 
 model.objective = Objective(
@@ -147,7 +190,8 @@ if __name__ == '__main__':
 
     opt = SolverFactory("gurobi_direct")
     opt.options["threads"] = 4
-    print('Solving')
+    print("========================= SOLVING MODEL =========================")
+
     results = opt.solve(model, tee=True)
 
     results.write()
